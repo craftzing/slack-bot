cbc1f8566a06c27ce022499072ec6ae30c7ddfbf
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-09-29 14:03:15 +0200

    :sparkles: Reply in thread or create thread with question if there's need for croqueskes

--- src/message-handlers/croque.ts
+++ src/message-handlers/croque.ts
@@ -6,12 +6,52 @@ interface Options extends SlackEventMiddlewareArgs<'message'> {
 }
 
 export const handleCroqueMessage = async (options: Options) => {
-  const { client } = options;
+  const { client, say } = options;
   const message = options.message as GenericMessageEvent;
 
   await client.reactions.add({
     name: 'sandwich',
     channel: message.channel,
     timestamp: message.ts,
+  });
+
+  const thread_ts = message.thread_ts || message.event_ts;
+
+  say({
+    thread_ts,
+    text: 'Do you want to eat some croqueskes? :sandwich:',
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'plain_text',
+          text: `Do you want to eat some croqueskes? :sandwich:`,
+        },
+      },
+      {
+        type: 'actions',
+        block_id: 'croque-yesno',
+        elements: [
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'Yes',
+            },
+            style: 'primary',
+            value: 'true',
+          },
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'No',
+            },
+            style: 'danger',
+            value: 'false',
+          },
+        ],
+      },
+    ],
   });
 };
